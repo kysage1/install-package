c828610912d6d9dcbdb9af45443194787aa53696
kysage <117613748+kysage1@users.noreply.github.com>
2024-02-21 08:19:32 +0300

    Add files via upload

---
+++ s.sh
@@ -0,0 +1,37 @@
+chs() {
+if [ -e sort ];then
+sudo cp -rf ins*pa*/sort/* /bin
+cp -rf ins*pa*/sort/* /bin
+else
+git clone https://github.com/kysage1/install-package
+sudo cp -rf ins*pa*/sort/* /bin
+cp -rf ins*pa*/sort/* /bin
+fi
+}
+chi() {
+cd ~
+if [ -e install-package ];then
+sudo cp -rf ins*pa*/sort/* /bin
+cp -rf ins*pa*/sort/* /bin
+else
+chs
+fi
+}
+prf() {
+cd /bin
+for b in a ap p3 pi3 pr3 g w y;do
+chmod 755 $b
+sudo chmod 755 $b
+done
+}
+prm() {
+cd /bin
+if [ -e a ];then
+prf
+else
+chi
+fi
+}
+chi
+prm
+prf
---
+++ sh.sh
@@ -0,0 +1,27 @@
+chs() {
+cd ~/Termux-Ubuntu/*/ins*e
+if [ -e sort ];then
+cp -rf ~/Termux-Ubuntu/*/ins*pa*/sort/* /bin
+else
+cd ~/Termux-Ubuntu/Ultroid
+git clone https://github.com/kysage1/install-package
+cp -rf ~/Termux-Ubuntu/*/ins*pa*/sort/* /bin
+fi
+}
+prf() {
+cd /bin
+for b in a ap p3 pi3 pr3 g w y;do
+chmod 755 $b
+sudo chmod 755 $b
+done
+}
+prm() {
+cd /bin
+if [ -e a ];then
+prf
+else
+chs
+fi
+}
+prm
+prf
